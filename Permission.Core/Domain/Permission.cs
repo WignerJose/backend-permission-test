55b80204e8021be7a1f89c202d1084345cd8b84f
Jose Wigner <jwlliully@fassil.com.bo>
2022-05-23 23:55:53 -0400

    the first commit

---
+++ Permission.Core/Domain/Permission.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permission.Core.Domain
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public String EmployeeName { get; set; } = String.Empty;
+        public String EmployeeLastName { get; set; } = String.Empty;
+        public PermissionType PermissionType { get; set; } = new PermissionType();
+        public DateTime PermissionDate { get; set; } 
+    }
+}
