0037727f44588d8df8210865867a0ae9878912ea
Jose Wigner <jwlliully@fassil.com.bo>
2022-05-24 00:28:24 -0400

    add coonection data base

---
+++ Permission.Infrastructure/Persistence/PermissionDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Permissions.Core.Domain;
+using Permissions.Infrastructure.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permissions.Infrastructure.Persistence
+{
+    public class PermissionDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public PermissionDbContext(IOptions<DataBaseConfig> dataBaseConfig)
+        {
+            _connectionString = dataBaseConfig.Value.ConnectionString;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public DbSet<Permission> Permission { get; set; }
+
+    }
+}
---
+++ Permission.Infrastructure/utils/DataBaseConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permissions.Infrastructure.utils
+{
+    public class DataBaseConfig
+    {
+        public String ConnectionString { get; set; } = String.Empty;
+
+    }
+}
